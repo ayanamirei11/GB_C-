fba750bfad178bc5d0e1779e6d228ff18470db08
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-04-26 19:47:49 +0700

    add example 2

---
+++ example002/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace example002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите имя");
+            string name=Console.ReadLine();
+            Console.WriteLine("привет "+ name+"!!!");
+        }
+    }
+}
