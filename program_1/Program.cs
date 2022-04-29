8d23b075341a3afd378289fea1fb9f9640e07fae
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-04-29 11:48:52 +0700

    add in  program 1 code

--- program_1/Program.cs
+++ program_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a,b;
+String str;
+
+Console.WriteLine("Введите число");
+str=Console.ReadLine();
+a=int.Parse(str);
+Console.WriteLine("Введите квадрат этого числа");
+str=Console.ReadLine();
+b=int.Parse(str);
+
+if (a*a==b){
+    Console.WriteLine("да");
+}
+else {
+    Console.WriteLine("нет");
+}
+Console.ReadLine();
