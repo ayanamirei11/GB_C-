c022473b37e07ff7254ec8707a64a35ed27d9d78
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-04-29 12:05:08 +0700

    add in  program 2 code

---
+++ program_2/Program.cs
@@ -0,0 +1,36 @@
+﻿int a,b;
+String str;
+
+Console.WriteLine("Введите число");
+str=Console.ReadLine();
+a=int.Parse(str);
+
+b=a%7;
+switch(b){
+    case 1:
+        Console.WriteLine("Понидельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("неправильные данные");
+        break;
+}
+
+Console.ReadLine();
