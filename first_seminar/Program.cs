49db0c52971f7b97582e4fb5b711c89225b290c4
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-04-29 11:26:06 +0700

    add first seminar

---
+++ first_seminar/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число:");
+String str=Console.ReadLine();
+int a = int.Parse(str);
+int b=a*a;
+Console.WriteLine("Квадрат этого числа: "+b);
+//Console.WriteLine(b);
+str = Console.ReadLine();
